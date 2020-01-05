fe4e56b42585916ffe57dbf22473d11a011027e8
Mustafa Alparslan <msdalp@gmail.com>
2020-01-05 14:04:50 +0300

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+
+To use it, run `npm install` then `npm run start`
+
+For production build, run `npm run build`
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style/main.less";
+
+class Welcome extends React.Component {
+    render () {
+        return (
+            <>
+            <h1 className="header">Default React boilerplate</h1>
+            </>
+        );
+    }
+}
+
+ReactDOM.render(<Welcome />, document.getElementById("root"));
